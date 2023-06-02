58193d52756c81ca2409abb3f63cd3d6689012a2
SnakeMM <snakeberry@163.com>
2023-06-02 10:50:00 +0800

    cuda

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base").to("cuda")
 
 app = FastAPI()
 
@@ -14,19 +14,13 @@ async def root():
     return {"introduction": "Get image caption from open source models"}
 
 @app.get("/caption/")
-async def getCaption(img_url: str, prompt: str = '', cuda: bool = True):
+async def getCaption(img_url: str, prompt: str = ''):
     image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
     
     if prompt:
-        if cuda:
-            inputs = processor(image, prompt, return_tensors="pt").to("cuda")
-        else:
-            inputs = processor(image, prompt, return_tensors="pt")
+        inputs = processor(image, prompt, return_tensors="pt").to("cuda")
     else:
-        if cuda:
-            inputs = processor(image, return_tensors="pt").to("cuda")
-        else:
-            inputs = processor(image, return_tensors="pt")
+        inputs = processor(image, return_tensors="pt").to("cuda")
 
     out = model.generate(**inputs)
     caption = processor.decode(out[0], skip_special_tokens=True);
