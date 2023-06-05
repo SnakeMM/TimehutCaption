ea50fc96e67f94241f5b6c0249a8e5c581b4af57
SnakeMM <snakeberry@163.com>
2023-06-05 16:26:27 +0800

    clip

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import os
 import requests
 import torch
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from transformers import Blip2Processor, Blip2ForConditionalGeneration
-from dotenv import load_dotenv
+from transformers import CLIPProcessor, CLIPModel
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 load_dotenv()
 
-useBIP = os.getenv("MODEL_BIP_ENABLED") == "True"
-useBIP2 = os.getenv("MODEL_BIP2_ENABLED") == "True"
+useBLIP = os.getenv("MODEL_BLIP_ENABLED") == "True"
+useBLIP2 = os.getenv("MODEL_BLIP2_ENABLED") == "True"
+useCLIP = os.getenv("MODEL_CLIP_ENABLED") == "True"
 
 # BLIP
-if (useBIP):
+if (useBLIP):
     processor1 = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
     model1 = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base").to(device)
 
 # BLIP2
-if (useBIP2):
+if (useBLIP2):
     processor2 = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
     model2 = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16).to(device)
+
+# CLIP
+if (useCLIP):
+    processor3 = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    model3 = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
 
 app = FastAPI()
 
@@ -37,7 +44,7 @@ async def getCaptionBlip(
 ):
     image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
 
-    if not useBIP:
+    if not useBLIP:
         return {"error": "model disabled"}
 
     if prompt:
@@ -59,7 +66,7 @@ async def getCaptionBlip2(
 ):
     image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
 
-    if not useBIP2:
+    if not useBLIP2:
         return {"error": "model disabled"}
 
     if prompt:
@@ -72,4 +79,25 @@ async def getCaptionBlip2(
 
     return {
         "caption": caption
+    }
+
+@app.get("/tags/clip/")
+async def getTagsClip(
+    img_url: str, 
+):
+    if not useCLIP:
+        return {"error": "model disabled"}
+
+    image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+
+    tags = ["dad","mom","baby","dog","cat"]
+    inputs = processor3(text=tags, images=image, return_tensors="pt", padding=True).to(device)
+    outputs = model3(**inputs)
+    
+    logits_per_image = outputs.logits_per_image
+    probs = logits_per_image.softmax(dim=1).tolist()[0]
+
+    return {
+        "tags": tags,
+        "probs": probs
     }
